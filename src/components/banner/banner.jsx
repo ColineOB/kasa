f9950a6437af1c6c12ddfefb65f0bbcd112c7740
ColineC <coline.cantelou@gmail.com>
2024-02-28 20:59:09 +0100

    banner

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,15 +1,26 @@
 import './banner.css'
+import {useLocation} from 'react-router-dom'
 
 function Banner() {
-    return (
-        <div className='banner'>
-        <div className='banner-filter'>
-        </div>
-        <div className='banner-container'></div>
+    const location = useLocation();
+    if (location.pathname === "/aPropos") {
+        return (
+            <div className='banner'>
+                <div className='banner-container_apropos'></div>
+            </div>
+        )
 
-            <p>Chez vous, partout et ailleurs</p>
-        </div>
-    )
+    } else {
+        return (
+            <div className='banner'>
+            <div className='banner-filter'>
+            </div>
+            <div className='banner-container'></div>
+    
+                <p>Chez vous, partout et ailleurs</p>
+            </div>
+        )
+    }
 }
  
 export default Banner
